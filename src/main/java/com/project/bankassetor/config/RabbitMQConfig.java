39f500c8274efb44e10cfce6258a61b6fc19ab86
hyeji <hyeji6742@naver.com>
2024-10-24 14:07:37 +0900

    feat: DeadLetterQueue 추가 #17

--- src/main/java/com/project/bankassetor/config/RabbitMQConfig.java
+++ src/main/java/com/project/bankassetor/config/RabbitMQConfig.java
@@ -12,7 +12,31 @@ public class RabbitMQConfig {
     // AccessLogQueue 정의 및 Dead Letter 설정
     @Bean
     public Queue mainQueue() {
-        return QueueBuilder.durable("accessLogQueue").build();
+        return QueueBuilder.durable("accessLogQueue")
+                .withArgument("x-dead-letter-exchange", "deadLetterExchange")
+                .withArgument("x-dead-letter-routing-key", "deadLetterQueue")
+                .build();
+    }
+
+    // AccessLogQueue 정의 및 Dead Letter 설정
+    @Bean
+    public Queue deadLetterQueue() {
+        return QueueBuilder.durable("deadLetterQueue").build();
+    }
+
+    // Dead Letter Exchange 정의
+    @Bean
+    public Exchange deadLetterExchange() {
+        return ExchangeBuilder.directExchange("deadLetterExchange").durable(true).build();
+    }
+
+    // Dead Letter Queue와 Dead Letter Exchange 바인딩
+    @Bean
+    public Binding deadLetterBinding() {
+        return BindingBuilder.bind(deadLetterQueue())
+                .to(deadLetterExchange())
+                .with("deadLetterQueue")
+                .noargs();
     }
 
     // RabbitTemplate에 메시지 컨버터 설정

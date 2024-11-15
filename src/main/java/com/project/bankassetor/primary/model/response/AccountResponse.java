558a8e52e5089197902da0bd60138cc2fe49b379
hyeji <hyeji6742@naver.com>
2024-11-15 19:41:22 +0900

    refactor: UI 수정 및 데이터 포맷 #32

--- src/main/java/com/project/bankassetor/primary/model/response/AccountResponse.java
+++ src/main/java/com/project/bankassetor/primary/model/response/AccountResponse.java
@@ -4,9 +4,12 @@ import com.project.bankassetor.primary.model.entity.Account;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.cglib.core.Local;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,16 +24,26 @@ public class AccountResponse {
     private String accountNumber;
 
     // 입출금 후 잔고
-    private BigDecimal balance;
+    private String balance;
 
     // 생성일자
-    private LocalDateTime createdAt;
+    private String createdAt;
+
+    private static String formatBalance(BigDecimal balance) {
+        DecimalFormat decimalFormat = new DecimalFormat("#,###"); // 소수점 둘째 자리까지 표시
+        return decimalFormat.format(balance);
+    }
+
+    private static String formatter(LocalDateTime localDateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss");
+        return localDateTime.format(formatter);
+    }
 
     public static AccountResponse of(Account account){
         return AccountResponse.builder()
                 .accountNumber(account.getAccountNumber())
-                .balance(account.getBalance())
-                .createdAt(account.getCreatedAt())
+                .balance(formatBalance(account.getBalance()))
+                .createdAt(formatter(account.getCreatedAt()))
                 .build();
     }
 
@@ -39,8 +52,8 @@ public class AccountResponse {
                 .map(account -> AccountResponse.builder()
                         .accountId(account.getId())
                         .accountNumber(account.getAccountNumber())
-                        .balance(account.getBalance())
-                        .createdAt(account.getCreatedAt())
+                        .balance(formatBalance(account.getBalance()))
+                        .createdAt(formatter(account.getCreatedAt()))
                         .build())
                 .collect(Collectors.toList());
     }
--- src/main/java/com/project/bankassetor/primary/model/response/CheckingTransactionHistoryResponse.java
+++ src/main/java/com/project/bankassetor/primary/model/response/CheckingTransactionHistoryResponse.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,16 +17,21 @@ import java.util.stream.Collectors;
 public class CheckingTransactionHistoryResponse {
 
     private LocalDateTime transactionTime;  // 거래 시간
-    private BigDecimal transactionAmount;  // 거래 금액
-    private BigDecimal balanceAfter;  // 거래 후 잔액
+    private String transactionAmount;  // 거래 금액
+    private String balanceAfter;  // 거래 후 잔액
     private String type;
+
+    private static String formatAmount(BigDecimal amount) {
+        DecimalFormat decimalFormat = new DecimalFormat("#,###");
+        return decimalFormat.format(amount);
+    }
 
     public static List<CheckingTransactionHistoryResponse> of(List<CheckingTransactionHistory> transactionHistories) {
         return transactionHistories.stream()
                 .map(checkingTransactionHistory -> CheckingTransactionHistoryResponse.builder()
                         .transactionTime(checkingTransactionHistory.getTransactionTime())
-                        .transactionAmount(checkingTransactionHistory.getTransactionAmount())
-                        .balanceAfter(checkingTransactionHistory.getBalance())
+                        .transactionAmount(formatAmount(checkingTransactionHistory.getTransactionAmount()))
+                        .balanceAfter(formatAmount(checkingTransactionHistory.getBalance()))
                         .type(checkingTransactionHistory.getTransactionType().getDisplayName())
                         .build())
                 .collect(Collectors.toList());
